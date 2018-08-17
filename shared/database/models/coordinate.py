2ef3cc678e163deba7045012a2800ec32f320548
Alex Porozov <36138075+AlexPrayer@users.noreply.github.com>
2018-08-17 10:59:18 +0500

    Update coordinate.py
    Fixed naming inside the peewee model

--- shared/database/models/coordinate.py
+++ shared/database/models/coordinate.py
@@ -16,16 +16,16 @@ class Coordinate(BaseModel):
         db_table = 'coordinates'
 
     id = PrimaryKeyField()
-    lat = DecimalField(max_digits=8, decimal_places=6)
-    long = DecimalField(max_digits=8, decimal_places=6)
+    latitude = DecimalField(max_digits=8, decimal_places=6)
+    longtitude = DecimalField(max_digits=8, decimal_places=6)
     date = DateTimeField(default=datetime.datetime.utcnow())
     user = ForeignKeyField(Employee, related_name='employee')
 
     def as_dict(self):
         return {
             'id': self.id,
-            'lat': str(self.lat),
-            'long': str(self.long),
+            'latitude': str(self.latitude),
+            'longtitude': str(self.longtitude),
             'user_id': self.user_id,
             'date': str(self.date),
             'user_name': self.user.name,
